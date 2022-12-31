51fbfc797584d670241a2a35011bb72aff41a2fc
Wixpor <110743466+Wixpor@users.noreply.github.com>
2022-12-30 19:42:43 -0500

    map destinos

--- src/MapDestino.js
+++ src/MapDestino.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import Destino from './Destino'
-import destinos from './destinos.json'
+import React from "react";
+import destinos from "./destinos.json";
 
 function MapDestino() {
   return (
     <div>
-      {destinos.destinossanandres.map(sanandres =>{
-      return(<div>
-      <h1>hotel={sanandres.hotel} descripcion={sanandres.descripcion} imagen={sanandres.imagen}</h1>
-      </div> )}
-      )}
+      {destinos.destinossanandres.map((sanandres) => {
+        return (
+          <div className="container">
+            <div className="col-sm-4 text-center">
+                <div className="card mt-4 bg-dark b border-white" style={{ width: "18rem"}}>
+                  <img
+                    src={sanandres.imagen}
+                    className="card-img-top"
+                    width="450"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title  text-light ">{sanandres.hotel}</h5>
+                    <p className="card-text text-light">{sanandres.descripcion}</p>
+                    <button variant="contained" className="btn btn-primary">
+                      {" "}
+                      Mas información
+                    </button>
+                  </div>
+                </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default MapDestino
+export default MapDestino;
