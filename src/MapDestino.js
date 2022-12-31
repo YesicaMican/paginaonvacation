d35ee9ef8ff82c5d2f2aa47b30667a6e99ffcffc
Wixpor <110743466+Wixpor@users.noreply.github.com>
2022-12-30 21:02:23 -0500

    YA SE PUEDEN SUBIR LAS TARJETAS CON SUS IMAGENES

--- src/MapDestino.js
+++ src/MapDestino.js
@@ -3,30 +3,96 @@ import destinos from "./destinos.json";
 
 function MapDestino() {
   return (
-    <div>
-      {destinos.destinossanandres.map((sanandres) => {
-        return (
-          <div className="container">
-            <div className="col-sm-4 text-center">
-                <div className="card mt-4 bg-dark b border-white" style={{ width: "18rem"}}>
-                  <img
-                    src={sanandres.imagen}
-                    className="card-img-top"
-                    width="450"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title  text-light ">{sanandres.hotel}</h5>
-                    <p className="card-text text-light">{sanandres.descripcion}</p>
-                    <button variant="contained" className="btn btn-primary">
-                      {" "}
-                      Mas información
-                    </button>
+    <div className="container">
+      <div className="row">
+        {destinos.destinossanandres.map((sanandres) => {
+          return (
+            <div className="col-sm-4">
+              <div className="card text-center mt-4">
+                <div id="carouselExampleIndicators" class="carousel slide">
+                  <div class="carousel-indicators">
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="0"
+                      class="active"
+                      aria-current="true"
+                      aria-label="Slide 1"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="1"
+                      aria-label="Slide 2"
+                    ></button>
+                    <button
+                      type="button"
+                      data-bs-target="#carouselExampleIndicators"
+                      data-bs-slide-to="2"
+                      aria-label="Slide 3"
+                    ></button>
                   </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img
+                        src={sanandres.imagen1}
+                        class="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                    <div class="carousel-item">
+                      <img
+                        src={sanandres.imagen2}
+                        class="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                    <div class="carousel-item">
+                      <img
+                        src={sanandres.imagen3}
+                        class="d-block w-100"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                  <button
+                    class="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev"
+                  >
+                    <span
+                      class="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Previous</span>
+                  </button>
+                  <button
+                    class="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next"
+                  >
+                    <span
+                      class="carousel-control-next-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span class="visually-hidden">Next</span>
+                  </button>
                 </div>
+                <div className="card-body">
+                  <h5 className="card-title">{sanandres.hotel}</h5>
+                  <p className="card-text ">{sanandres.descripcion}</p>
+                  <button variant="contained" className="btn btn-primary">
+                    {" "}
+                    Mas información
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
